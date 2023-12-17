06a1be18d01112f77407950146c3510efbf50cd0
ArsonistChoir <haiden2004ellis@outlook.com>
2023-12-17 00:37:58 -0600

    Converted about me into a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import AboutMe from "./components/AboutMe";
 
 function App () {
 
     return(
         <div>
             <Header />
+            <AboutMe />
+            <Footer />
         </div>
     )
 };
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe () {
+   return (
+        <div id="about-me">
+            <h2>About Me</h2>
+            <p>I'm an aspiring developing learning in to code. I have experience in HTML, CSS, Javascript, Node.js, and more! I got into coding for the same reasons I got a computer, I like learning more about the technology I have and how to use it.</p>
+        </div>
+   ) 
+};
+
+export default AboutMe;
