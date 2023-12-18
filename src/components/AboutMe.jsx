7931fa1710dbecc5aee912ae0e74a0b42dbb61c0
ArsonistChoir <haiden2004ellis@outlook.com>
2023-12-17 22:15:33 -0600

    updated about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,11 @@ function AboutMe () {
    return (
         <div id="about-me">
             <h2>About Me</h2>
-            <p>I'm an aspiring developing learning in to code. I have experience in HTML, CSS, Javascript, Node.js, and more! I got into coding for the same reasons I got a computer, I like learning more about the technology I have and how to use it.</p>
+            <p id='about-me-text'>I'm an aspiring developing learning in to code. I have experience in HTML, CSS, Javascript, Node.js, and more! I took a coding bootcamp from Northwestern University.
+            <br />
+            <br />
+            <p>I started coding for the same reason I started modoifying my computer, I like to know how work and how to mess with them. I remeber in class looking in inspect and not knowing anything going on. I'm happy to say I not only under that now but much more.</p>
+            </p>
         </div>
    ) 
 };
