e16142434a1bb0810c36e3ac07df2885e485f38e
ArsonistChoir <haiden2004ellis@outlook.com>
2023-12-17 21:28:09 -0600

    Got all images and links setup just need to do style

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,8 @@ function Projects() {
     const projects = [
         { title: "Trip Tix", link: "https://chart099.github.io/Bootcamp-Project-1/", imageSrc: "./src/assets/Screenshot 2023-12-17 at 21-02-25 Document.png" },
         { title: "Review a Movie", link: "https://spooky-ghost-09826-e1fe8c97ce3e.herokuapp.com/", imageSrc: "./src/assets/Screenshot 2023-12-17 at 21-10-01 Review a Movie.png" },
-        { title: "Project 3", link: "https://example.com/project3", imageSrc: "https://www.example.com/image3.jpg" },
-        { title: "Project 4", link: "https://example.com/project4", imageSrc: "https://www.example.com/image4.jpg" },
+        { title: "Big City Jobs", link: "https://cryptic-gorge-45528-e8e049cdd22a.herokuapp.com/", imageSrc: "./src/assets/Screenshot 2023-12-17 at 21-22-01 Big City Jobs.png" },
+        { title: "Horiseon SEO", link: "https://arsonistchoir.github.io/Horiseon-SEO/", imageSrc: "./src/assets/Screenshot 2023-12-17 at 21-25-34 Horiseon.png" },
     ];
 
     return (
